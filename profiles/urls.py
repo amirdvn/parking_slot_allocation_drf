4795cadfdbbe9aabfd2f2fc79111573c63880542
Amir <amir.dvn.82@gmail.com>
2026-09-02 14:43:08 +0330

    refactor(profile): refactor user profile view and update endpoint
    - Change`ProfileView` (APIView) to `ProfileUpdateView` (RetrieveUpdateAPIView)
    - Added `ProfileUpdateView` (RetrieveUpdateAPIView) to allow the user to view and update their own profile
    - ProfileUpdateView view are restricted with `IsAuthenticated` permission
    - Registered new URL routes for `profile/`, `profile/me/`, and `profile/update/`

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ProfileView.as_view(), name='profile')]
+    path('me/', ProfileView.as_view(), name='profile_me'),
+    path('me/update/', ProfileView.as_view(), name='profile_update'),
+
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,10 @@
-from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-
 from .serializers import ProfileSerializer
 
 
-class ProfileView(APIView):
+class ProfileView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        serializer = ProfileSerializer(request.user)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    serializer_class = ProfileSerializer
+    def get_object(self):
+         return self.request.user
